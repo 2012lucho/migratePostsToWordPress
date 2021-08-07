8598fe08f743ab52d79c84877a5f8b9e66d31a75
berty <lucho.2012.tandil@gmail.com>
2021-08-06 23:09:34 -0300

    Se crea nuevo registro post meta

--- WordPressImporter.php
+++ WordPressImporter.php
@@ -215,8 +215,15 @@ class WordPressImporter {
           'post_mime_type'        => 'image/jpeg',
           'comment_count'         => 0,
           'imagen'                => $post['imagen'],
-        ]);
+        ], $post_f);
 
+        if ($post_image_id != 0){
+          $this->insertPostMeta([
+            'post_id'    => $post_f,
+            'meta_key'   => '_thumbnail_id',
+            'meta_value' => $post_image_id,
+          ]);
+        }
       }
     } else {
       print("   A Post procesado, ID> ".$post_f."\n");
@@ -283,14 +290,14 @@ class WordPressImporter {
   ///// IMÄGENES Y METADATA
   //////////////////////
 
-  public function insertImage( $image_data ){
+  public function insertImage( $image_data, $post_id ){
     print("   D Descargando imagen de POST, ID> ".$image_data['imagen']."\n");
 
     if ( strpos( $image_data['imagen'], $this->importer_config['SITE_URL'] ) === false ){
       print("   A El recurso no se pertenece al sitio ID> ".$image_data['imagen']."\n");
     } else {
       //se inserta la imagen como un nuevo post
-      $this->insertPostElement( $image_data );
+      $post_image_id = $this->insertPostElement( $image_data );
 
       //Creación de estructura de directorios de acuerdo a la fecha
       $strTime = strtotime($image_data['post_date']);
@@ -303,6 +310,7 @@ class WordPressImporter {
 
       if ( file_exists($rutaImg) ){
         print("   = El recurso ya fue descargado! > ".$fileName."\n");
+        return $post_image_id;
       } else {
 
         //se hace peticion curl para obtener la imagen
@@ -322,14 +330,14 @@ class WordPressImporter {
         //Se verifica que la ruta se corresponda a una imagen
         $extension_explode = explode( ".", $fileName );
         $extension_explode = array_pop($extension_explode);
-        if ($curlDatos !== false && ($extension_explode == 'gif' || $extension_explode == 'jpeg' || $extension_explode == 'jpg' || $extension_explode == 'png' || $extension_explode == 'webm') ){
+        if ($curlDatos !== false && ($extension_explode == 'gif' || $extension_explode == 'JPG' || $extension_explode == 'jpeg' || $extension_explode == 'jpg' || $extension_explode == 'png' || $extension_explode == 'webm') ){
           $miarchivo  = fopen($rutaImg, "w+");
 
           // Insertamos en la carpeta la imagen
           fputs($miarchivo, $curlDatos);
           fclose($miarchivo);
 
-          //Si llegamos aca sin problemas, se agrega el registro en la tabla wp_postmeta
+          return $post_image_id;
         } else {
           print("   Err El recurso no se trata de una imagen! o no hay conexion a internet ?¿> ".$fileName."\n");
         }
@@ -337,7 +345,42 @@ class WordPressImporter {
       }
 
     }
+    return 0;
+  }
 
+  public function insertPostMeta( $postMeta ){
+    $post_meta_id = $this->getPostMetaByPostId( $postMeta['post_id'] );
+    if ( $post_meta_id == 0 ){
+      print("   + Se agregó registro post_meta "."\n");
+      $table_name   = $this->importer_config['POST_META']['TABLE_NAME'];
+      $sql          = "INSERT INTO `$table_name` (post_id, meta_key, meta_value) VALUES (:post_id, :meta_key, :meta_value)";
+      $query        = $this->db_destino->prepare( $sql );
+      $query->execute([
+        ':post_id'    => $postMeta['post_id'],
+        ':meta_key'   => $postMeta['meta_key'],
+        ':meta_value' => $postMeta['meta_value'],
+      ]);
+      return $this->db_destino->lastInsertId();
+    } else {
+      print("   = registro post_meta existente"."\n");
+      return $post_meta_id;
+    }
+  }
+
+  public function getPostMetaByPostId( $post_id ){
+    $table_name     = $this->importer_config['POST_META']['TABLE_NAME'];
+    $post_id_field  = $this->importer_config['POST_META']['POST_ID_FIELD'];
+    $sql          = "SELECT * FROM `$table_name` WHERE `$post_id_field` = :post_id AND meta_key='_thumbnail_id'";
+    $query        = $this->db_destino->prepare( $sql );
+    $query->execute([':post_id'=> $post_id]);
+    $query = $query->fetchAll(PDO::FETCH_OBJ);
+
+    if ( count($query) > 0){
+      $query = get_object_vars($query[0]);
+      return $query[ $this->importer_config['POST_META']['POST_ID_FIELD'] ];
+    } else {
+      return 0;
+    }
   }
 
   ///////////////////////////////////////////////////////////////////
